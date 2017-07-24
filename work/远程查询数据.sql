8f2bde66bc4c4a119d164b73db841ab3e91f8680
caolh <444261689@qq.com>
2017-07-24 14:45:39 +0800

    表变量及存储过程应用

--- work/远程查询数据.sql
+++ work/远程查询数据.sql
@@ -1,8 +1,8 @@
-exec sp_addlinkedserver '169yichePeccancy', '', 'SQLOLEDB', '192.168.0.169\sql2008'
-exec sp_addlinkedsrvlogin '169yichePeccancy', 'false',null, 'sa', 'sa' 
+exec sp_addlinkedserver '169', '', 'SQLOLEDB', '192.168.0.169\sql2008'
+exec sp_addlinkedsrvlogin '169', 'false',null, 'sa', 'sa' 
 
 
---select * from [169yichePeccancy].数据库名.dbo.表名 
+--select * from [169].数据库名.dbo.表名 
 
 --以后不再使用时删除链接服务器
 exec sp_dropserver 'ITSV ', 'droplogins' 
